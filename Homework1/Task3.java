0f6c8272abd8c63798fb503ca4ff13656d9a5021
Aleksey <mybox20134@gmail.com>
2022-11-11 11:22:30 +0200

    added folder <Homework1>, fixed Tasks 2,3,7

---
+++ Homework1/Task3.java
@@ -0,0 +1,7 @@
+public class Task3 {
+    public static void main(String[] args) {
+        int calcium = 12;
+        int fahrenheit = (calcium * 9 / 5) + 32;
+        System.out.println(fahrenheit + "°F");
+    }
+}
--- Task3.java
+++
@@ -1,7 +0,0 @@
-public class Task3 {
-    public static void main(String[] args) {
-        int Calcium = 12;
-        int Fahrenheit = (Calcium * 9 / 5) + 32;
-        System.out.println(Fahrenheit + "°F");
-    }
-}
