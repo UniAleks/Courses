0b168445068dcc4304f8ccf974b6db715e74e3e5
UniAleks <105492511+UniAleks@users.noreply.github.com>
2022-11-22 12:18:32 +0200

    Update TaskFour.java

--- Homework2/TaskFour.java
+++ Homework2/TaskFour.java
@@ -4,7 +4,10 @@ public class TaskFour {
     public static void main(String[] args) {
         int mas[] = {1, 3, 6, 9, 4, 5, 9, 11, 43, 76};
         int square = 2;
+        for (int i = 0;i < mas.length;i++){
+            mas[i]= (int) Math.pow(mas[i],square );
+        }
         int sum = IntStream.of(mas).sum();
-        System.out.println(Math.pow(sum,square));
+        System.out.println(sum);
     }
 }
